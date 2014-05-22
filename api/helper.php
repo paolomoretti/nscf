6847881bda79abb9d5e336ba3a511a90b6921492
Paolo Moretti <littlebrown@gmail.com>
2014-05-22 14:45:07 +0100

    APC Api improvements

--- api/helper.php
+++ api/helper.php
@@ -1,7 +1,12 @@
 <?php
 
+Flight::map("toKeyValue", function ($text) {
+  return str_replace(" ", "_", $text);
+});
+
 Flight::map("query", function ($sql) {
-  Flight::etag(date("Y-m-d"));
+  if ($entries = apc_fetch(Flight::toKeyValue($sql)))
+    return $entries;
 
   $context  = stream_context_create(array(
     'http' =>	array(
@@ -10,6 +15,5 @@ Flight::map("query", function ($sql) {
       'content' => http_build_query( array('sql'=>$sql))
     )
   ));
-
   return json_decode(file_get_contents("http://www.nonsocosafare.it/api/run.asp", true, $context));
 });
