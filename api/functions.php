2ab415c9598f976c9ea38195e8ece2e3deb3cd4c
Paolo Moretti <littlebrown@gmail.com>
2014-05-23 02:24:31 +0100

    api update

--- api/functions.php
+++ api/functions.php
@@ -4,13 +4,12 @@ Flight::map("toKeyValue", function ($text) {
   return str_replace(" ", "_", $text);
 });
 
-Flight::map("cache", function ($key, $callback, $expire = null) {
+Flight::map("cache", function ($key, $callback, $expire) {
   if ($entries = apc_fetch(md5($key)))
     return $entries;
 
   $return = $callback($key);
-  apc_add(md5($key), $return, ($expire = null) ? 60*60*2 : $expire);  // Cache for 2 hours
-
+  apc_add(md5($key), $return, ($expire == null) ? 60*60*2 : $expire);  // Cache for 2 hours
   return $return;
 
 });
@@ -18,7 +17,7 @@ Flight::map("cache", function ($key, $callback, $expire = null) {
 Flight::map("query", function ($sql) {
   return Flight::cache($sql, function ($sql) {
     $context  = stream_context_create(array(
-      'http' =>	array(
+      'http' => array(
         'method'  => 'POST',
         'header'  => 'Content-type: application/x-www-form-urlencoded',
         'content' => http_build_query( array('sql'=>$sql))
