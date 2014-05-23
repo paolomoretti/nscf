d6f80e7422421048c8ac39fb04a55df50893f482
root <root@bitterbrown.com>
2014-05-23 17:23:11 +0200

    api eventi weekend

--- api/config.php
+++ api/config.php
@@ -15,13 +15,13 @@ header('Content-type: application/json');
 Flight::set("event_fields", "id, NomeEventoSuperEsteso as nome, data, EM, ES, FI, MM, SP, FE, NSCF_Link,
 
           Luoghi_Web_Link as luogo_link, Luoghi_Facebook_Link as luogo_facebook, Luoghi_Myspace_Link as luogo_myspace, Luoghi_Twitter_Link as luogo_twitter,
-                                        Luoghi_GooglePlus_Link as luogo_googleplus, Luoghi_Foursquare_Link as luogo_foursquare, Luoghi_JPG as luogo_image, luogo, Indirizzo$
+          Luoghi_GooglePlus_Link as luogo_googleplus, Luoghi_Foursquare_Link as luogo_foursquare, Luoghi_JPG as luogo_image, luogo, Indirizzo,
 
-          Evento_Myspace_Link as evento_myspace, Evento_Twitter_Link as evento_twitter, Evento_GooglePlus_Link as evento_googleplus, Evento_Foursquare_Link as evento_fours$
-          Evento_Web_Link as evento_link, Evento_Facebook_Link as evento_faecbook,
+          Evento_Myspace_Link as evento_myspace, Evento_Twitter_Link as evento_twitter, Evento_GooglePlus_Link as evento_googleplus, Evento_Foursquare_Link as evento_foursquare,
+          Evento_Web_Link as evento_link, Evento_Facebook_Link as evento_facebook,
 
-                                        ID_ilMeteo as meteo_id,
-                                        JPG_Miglior_Link as image_hi, DataInizio, DataFine, OraInizio, Annullato, OraFine, GPS_L, NomeLuogo, Localita");
+          ID_ilMeteo as meteo_id,
+          JPG_Miglior_Link as image_hi, DataInizio, DataFine, OraInizio, Annullato, OraFine, GPS_L, NomeLuogo, Localita");
 
 
 
